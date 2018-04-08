bcff5fbd97b7c2b2b08ab8d094f20641d825c3c1
MrRaindrop <tekkahs@gmail.com>
2018-04-08 15:49:05 +0800

    update postcss-plugin-weex & adjust order of postcss plugins array.

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,11 +3,11 @@ const precompile = require('weex-vue-precompiler')()
 module.exports = {
   optimizeSSR: false,
   postcss: [
-    require('postcss-plugin-weex')(),
     require('autoprefixer')({
       browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
     }),
-    require('postcss-plugin-px2rem')({ rootValue: 75, minPixelValue: 1.01 })
+    require('postcss-plugin-px2rem')({ rootValue: 75, minPixelValue: 1.01 }),
+    require('postcss-plugin-weex')()
   ],
   compilerModules: [
     {
